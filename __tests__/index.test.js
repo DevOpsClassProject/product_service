f71ba68f56e2c2d70c2f4369c3e4fce682127755
ellie <ellie@yoonchae.mshome.net>
2026-03-11 15:37:17 -0500

    upload configuarion files for product service

---
+++ __tests__/index.test.js
@@ -0,0 +1,6 @@
+// A simple smoke test to ensure the environment is working
+describe('Basic Math Test', () => {
+  it('should confirm that 1 + 1 is 2', () => {
+    expect(1 + 1).toBe(2);
+  });
+});
